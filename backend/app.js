79b1d82446732becdea1266d894b39fcf8b68625
= <divyanshdh@gmail.com>
2023-11-18 17:57:04 +0530

    deploy commit

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,7 @@ const corsOptions = {
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'http://127.0.0.1',
+        'https://outlay-tracker-ft.netlify.app/'
     ],
     credentials: true,
     exposedHeaders: ['set-cookie'],
